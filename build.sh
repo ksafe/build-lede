fb15c61a8739d7caea20f9b383bdac27dd3d0319
ksafe <ksafer@gmail.com>
2020-06-10 15:44:11 +0000

    modify OpenClash & remove netdata

--- build.sh
+++ build.sh
@@ -17,10 +17,29 @@ git_pull "https://github.com/coolsnowwolf/lede.git" "lede" "reset"
 
 mkdir -p lede/package/ksafe
 cd lede/package/ksafe
+
+# AdGuardHome
 git_pull "https://github.com/rufengsuixing/luci-app-adguardhome" "luci-app-adguardhome"
+
+# OpenClash
+rm -rf OpenClash
 git_pull "-b master https://github.com/vernesong/OpenClash" "OpenClash"
+mkdir -p OpenClash/luci-app-openclash/files/etc/openclash/{core,config}
+wget -qc https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -O - | tar xz -C OpenClash/luci-app-openclash/files/etc/openclash/core
+wget -qc http://192.168.88.88:9990/DlerCloud-Trojan.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config
+wget -qc http://192.168.88.88:9990/ksafe.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config
+
+# luci-theme-argon
 rm -rf ../lean/luci-theme-argon
-git_pull "-b 18.06 https://github.com/jerrykuku/luci-theme-argon.git" luci-theme-argon
+#rm -rf luci-theme-argon
+git_pull "-b 18.06 https://github.com/jerrykuku/luci-theme-argon.git" "luci-theme-argon"
+
+# luci-app-vss-plus
+git_pull "https://github.com/fw876/helloworld.git" "helloworld"
+
+# luci-app-vssr
+#git_pull "https://github.com/jerrykuku/luci-app-vssr.git" "luci-app-vssr"
+
 cd ../..
 
 ./scripts/feeds update -a
