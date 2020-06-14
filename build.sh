c3460192b7bb113df688b85484c78282f4be606b
ksafe <ksafer@gmail.com>
2020-06-14 15:32:40 +0000

    Download OpenClash Core & Set theme to Argon

--- build.sh
+++ build.sh
@@ -22,12 +22,23 @@ cd lede/package/ksafe
 git_pull "https://github.com/rufengsuixing/luci-app-adguardhome" "luci-app-adguardhome"
 
 # OpenClash
-rm -rf OpenClash
+#rm -rf OpenClash
 git_pull "-b master https://github.com/vernesong/OpenClash" "OpenClash"
 mkdir -p OpenClash/luci-app-openclash/files/etc/openclash/{core,config}
 wget -qc https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -O - | tar xz -C OpenClash/luci-app-openclash/files/etc/openclash/core
-wget -qc http://192.168.88.88:9990/DlerCloud-Trojan.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config
-wget -qc http://192.168.88.88:9990/ksafe.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config
+wget -qc http://192.168.88.88:9990/DlerCloud-Trojan.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://192.168.88.88:9990/ksafe.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+echo "
+- IP-CIDR,34.92.26.211/32,DIRECT
+- IP-CIDR,38.106.20.173/32,DIRECT
+- IP-CIDR,45.131.68.95/32,DIRECT
+- DOMAIN,1229.io,DIRECT
+- DOMAIN,ksafe.cn,DIRECT
+- DOMAIN,ksafe.org,DIRECT
+- DOMAIN-SUFFIX,1229.io,DIRECT
+- DOMAIN-SUFFIX,ksafe.cn,DIRECT
+- DOMAIN-SUFFIX,ksafe.org,DIRECT
+" >> OpenClash/luci-app-openclash/files/etc/openclash/custom/openclash_custom_rules.list
 
 # luci-theme-argon
 rm -rf ../lean/luci-theme-argon
@@ -41,6 +52,8 @@ git_pull "https://github.com/fw876/helloworld.git" "helloworld"
 #git_pull "https://github.com/jerrykuku/luci-app-vssr.git" "luci-app-vssr"
 
 cd ../..
+
+sed -i '/uci commit luci/ i uci set luci.main.mediaurlbase=''\/luci-static\/argon''' package/lean/default-settings/files/zzz-default-settings
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cur_time=$(date '+%Y%m%d%H%M')
+#cur_time=$(date '+%Y%m%d%H%M')
+cur_time=$(date '+%Y%m%d')
 bin_dir="lede/bin/targets/x86/64"
 rsync -avP ${bin_dir}/config.seed 192.168.88.88:~/OpenWRT/Lean/${cur_time}/
 rsync -avP ${bin_dir}/openwrt-x86-64-combined-squashfs.* 192.168.88.88:~/OpenWRT/Lean/${cur_time}/
