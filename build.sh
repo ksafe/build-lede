86ed915e724d4a0e7f5c525ced07bfeedf71086e
ksafe <ksafer@gmail.com>
2020-07-19 20:43:29 +0800

    update

--- build.sh
+++ build.sh
@@ -13,22 +13,26 @@ git_pull(){
 	fi
 }
 
+# sudo apt update
+# sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget swig
+
 git_pull "https://github.com/coolsnowwolf/lede.git" "lede" "reset"
 
 mkdir -p lede/package/ksafe
 cd lede/package/ksafe
-
-# AdGuardHome
-rm -rf luci-app-adguardhome
-#git_pull "https://github.com/rufengsuixing/luci-app-adguardhome" "luci-app-adguardhome"
 
 # OpenClash
 #rm -rf OpenClash
 git_pull "-b master https://github.com/vernesong/OpenClash" "OpenClash"
 mkdir -p OpenClash/luci-app-openclash/files/etc/openclash/{core,config}
 wget -qc https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -O - | tar xz -C OpenClash/luci-app-openclash/files/etc/openclash/core
-wget -qc http://192.168.88.88:9990/DlerCloud-Trojan.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
-wget -qc http://192.168.88.88:9990/ksafe.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/ksafe.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/dler.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/dler-ss.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/dler-trojan.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/dler-v2Ray.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/jms.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
+wget -qc http://clash.ksafe.cn/renzhe.yaml -P OpenClash/luci-app-openclash/files/etc/openclash/config/
 echo "
 - IP-CIDR,34.92.26.211/32,DIRECT
 - IP-CIDR,38.106.20.173/32,DIRECT
@@ -39,24 +43,21 @@ echo "
 - DOMAIN-SUFFIX,1229.io,DIRECT
 - DOMAIN-SUFFIX,ksafe.cn,DIRECT
 - DOMAIN-SUFFIX,ksafe.org,DIRECT
-" >> OpenClash/luci-app-openclash/files/etc/openclash/custom/openclash_custom_rules.list
+" > OpenClash/luci-app-openclash/files/etc/openclash/custom/openclash_custom_rules.list
 
 # luci-theme-argon
-rm -rf ../lean/luci-theme-argon
-#rm -rf luci-theme-argon
-git_pull "-b 18.06 https://github.com/jerrykuku/luci-theme-argon.git" "luci-theme-argon"
+#rm -rf ../lean/luci-theme-argon
+#git_pull "-b 18.06 https://github.com/jerrykuku/luci-theme-argon.git" "luci-theme-argon"
 
 # luci-app-vss-plus
 git_pull "https://github.com/fw876/helloworld.git" "helloworld"
-
-# luci-app-vssr
-#git_pull "https://github.com/jerrykuku/luci-app-vssr.git" "luci-app-vssr"
 
 cd ../..
 
 sed -i '/uci commit luci/ i uci set luci.main.mediaurlbase=''\/luci-static\/argon''' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/''OpenWrt ''/''OpenWrt_$(date '+%Y%m%d') ''/' package/lean/default-settings/files/zzz-default-settings
 
+rm -rf ./bin/targets
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
--- rsync.sh
+++ rsync.sh
@@ -2,5 +2,4 @@
 #cur_time=$(date '+%Y%m%d%H%M')
 cur_time=$(date '+%Y%m%d')
 bin_dir="lede/bin/targets/x86/64"
-#rsync -avP ${bin_dir}/config.seed 192.168.88.13:/disk/OpenWRT/${cur_time}/
-rsync -avP ${bin_dir}/{config.seed,openwrt-x86-64-combined-squashfs.*} 192.168.88.13:/disk/OpenWRT/${cur_time}/
+rsync -avP ${bin_dir}/* 192.168.88.13:/disk/OpenWRT/${cur_time}/
